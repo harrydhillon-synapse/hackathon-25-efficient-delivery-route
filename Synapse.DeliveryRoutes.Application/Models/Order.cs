5e8f2768c23b79bf31a165a56a937a7e0951ffd4
uladzislaukisialiou <uladzislau.kisialiou@synapsehealth.com>
2025-04-16 21:38:14 +0200

    Updated data files with new data (#2)
    * Updated data files with new data
    
    * Update models to match the JSON files

--- Synapse.DeliveryRoutes.Application/Models/Order.cs
+++ Synapse.DeliveryRoutes.Application/Models/Order.cs
@@ -5,8 +5,25 @@ public class Order
     public required string OrderId { get; set; }
     public required string PatientName { get; set; }
     public required string Address { get; set; }
-    public required GeoCoordinate Coordinates { get; set; }
+    public required GeoCoordinate GeoCoordinates { get; set; }
     public required string PreferredTimeWindow { get; set; }
     public List<string> ProductIds { get; set; } = [];
-    public DateTime DeliveryDeadline { get; set; }
+    public required string DeliveryDeadline { get; set; }
+    public required string Priority { get; set; }
+    public required DeliveryNotes DeliveryNotes { get; set; }
+    public required OrderStatus Status { get; set; }
+}
+
+public class DeliveryNotes
+{
+    public required string Accessibility { get; set; }
+    public required string SetupRequirements { get; set; }
+    public required string SpecialInstructions { get; set; }
+}
+
+public class OrderStatus
+{
+    public required string CurrentStatus { get; set; }
+    public DateTime? LastUpdated { get; set; }
+    public string? Notes { get; set; }
 }
