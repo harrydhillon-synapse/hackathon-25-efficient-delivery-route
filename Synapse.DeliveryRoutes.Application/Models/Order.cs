b8a0d11eef45b09f45ef956844aa11b465596dde
Nathan LaFratta <nathan.lafratta@synapsehealth.com>
2025-04-17 16:47:27 -0400

    fixed use of TimeWindows

--- Synapse.DeliveryRoutes.Application/Models/Order.cs
+++ Synapse.DeliveryRoutes.Application/Models/Order.cs
@@ -7,7 +7,7 @@ public class Order
     public required string PatientPhone { get; set; }
     public required string Address { get; set; }
     public required GeoCoordinates Location { get; set; }
-    public required TimeWindow PreferredDeliveryTimeWindow { get; set; }
+    public required TimeWindow[] AvailableTimes { get; set; }
     public List<string> ProductIds { get; set; } = [];
     public DateOnly DeliveryDeadline { get; set; }
     public required OrderPriority Priority { get; set; }
