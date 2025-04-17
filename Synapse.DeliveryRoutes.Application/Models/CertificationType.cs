74dcf8f735b12233b57e9b518a908dfc042049a6
Nathan LaFratta <nathan.lafratta@synapsehealth.com>
2025-04-17 10:23:42 -0400

    Contiuning the refactor for better data

--- Synapse.DeliveryRoutes.Application/Models/CertificationType.cs
+++ Synapse.DeliveryRoutes.Application/Models/CertificationType.cs
@@ -1,13 +1,10 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Synapse.DeliveryRoutes.Application.Models;
+﻿namespace Synapse.DeliveryRoutes.Application.Models;
 
 public enum CertificationType
 {
     Basic = 1,
     Respiratory = 2,
     Mobility = 3,
-    [JsonPropertyName("Hospital Beds")]
     HospitalBeds = 4,
     Complex = 5
 }
