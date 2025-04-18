e7283c21b862a7fb498164d41da3a05db224e5cb
pmayfield <patrick.mayfield@synapsehealth.com>
2025-04-18 16:31:23 -0500

    updating with cors

--- Synapse.DeliveryRoutes.Api/Program.cs
+++ Synapse.DeliveryRoutes.Api/Program.cs
@@ -3,6 +3,18 @@ using Synapse.DeliveryRoutes.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add CORS policy
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFlutterApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:52129") 
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddSwaggerGen(options =>
 {
@@ -29,8 +41,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Enable CORS
+app.UseCors("AllowFlutterApp");
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Use(async (context, next) =>
+{
+    await next();
+
+    // Debug: log all response headers
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    foreach (var header in context.Response.Headers)
+    {
+        logger.LogInformation("{Key}: {Value}", header.Key, header.Value);
+    }
+});
 
 app.Run();
